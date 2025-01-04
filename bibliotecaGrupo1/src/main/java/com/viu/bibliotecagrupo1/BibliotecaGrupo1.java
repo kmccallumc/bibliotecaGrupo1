c4acb1941a3d7fc806d3d912ed66a171a37afc4d
kmcca <kmcca@Kathleen-NewHP>
2025-01-04 13:27:33 +0100

    21GIIN-Grupo1-Biblioteca.Estructura inicial

---
+++ bibliotecaGrupo1/src/main/java/com/viu/bibliotecagrupo1/BibliotecaGrupo1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.viu.bibliotecagrupo1;
+
+/**
+ *
+ * @author kmccallum <Kathleen_McCallum VIU>
+ */
+public class BibliotecaGrupo1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
