df2bae993d7e02d36cc4867635e593199c250a34
brosskko <oscar.guardia23@gmail.com>
2025-01-06 12:16:10 +0100

    Update usuario.java

--- bibliotecaGrupo1/src/main/java/com/viu/bibliotecagrupo1/entitiyLayer/usuario.java
+++ bibliotecaGrupo1/src/main/java/com/viu/bibliotecagrupo1/entitiyLayer/usuario.java
@@ -4,10 +4,34 @@
  */
 package com.viu.bibliotecagrupo1.entitiyLayer;
 
+import java.util.Date;
+import java.util.Objects;
+
 /**
- *
+ * Clase que representa a un usuario del sistema de biblioteca
  * @author kmccallum <Kathleen_McCallum VIU>
  */
-public class usuario {
-    
-}
+public class Usuario {
+    private int id;
+    private String nombre;
+    private String apellidos;
+    private String dni;
+    private String email;
+    private String telefono;
+    private String direccion;
+    private Date fechaAlta;
+    private boolean activo;
+    private int numeroPrestamosPendientes;
+    private static final int MAX_PRESTAMOS = 3;
+
+    /* no se cuanto poner de max de prestamo?? 3? 4?     */
+    public Usuario(int id, String nombre, String apellidos, String dni, String email) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.dni = dni;
+        this.email = email;
+        this.fechaAlta = new Date();
+        this.activo = true;
+        this.numeroPrestamosPendientes = 0;
+    }
